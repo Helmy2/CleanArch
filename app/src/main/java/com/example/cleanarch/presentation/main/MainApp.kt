6aee77c3778c3c593437406f23406306eff1fc29
Helmy2 <mohelmy.eng@gmail.com>
2025-01-22 12:56:37 +0200

    feat: Implement profile screen and bottom navigation
    This commit introduces the profile screen and bottom navigation to the application:
    
    - Added a new ProfileRoute for displaying the profile screen.
    - Updated MainNavigation to support navigation to the ProfileRoute.
    - Introduced TopLevelRoute data class to represent top-level navigation routes.
    - Added a Scaffold to the MainApp to display the bottom navigation bar.
    - Updated the MainApp to use the rememberNavController for navigation.
    - Added logic to display the bottom navigation bar only for top-level routes.
    - Implemented navigation logic for top-level routes using the navigate method.
    - Updated the MainNavigation composable to accept navController and startDestination as parameters.

--- app/src/main/java/com/example/cleanarch/presentation/main/MainApp.kt
+++ app/src/main/java/com/example/cleanarch/presentation/main/MainApp.kt
@@ -1,14 +1,82 @@
 package com.example.cleanarch.presentation.main
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.cleanarch.presentation.theme.CleanArchTheme
 import org.koin.androidx.compose.KoinAndroidContext
 
+
+data class TopLevelRoute<T : Any>(val name: String, val route: T, val icon: ImageVector)
+
+val topLevelRoutes =
+    listOf(
+        TopLevelRoute("Home", AppDestination.Home, Icons.Default.Home),
+        TopLevelRoute(
+            "Profile", AppDestination.Profile, Icons.Default.AccountCircle
+        ),
+    )
+
 @Composable
 fun MainApp() {
+    val navController = rememberNavController()
+
     KoinAndroidContext {
         CleanArchTheme {
-            MainNavigation()
+            Scaffold(
+                bottomBar = {
+                    val navBackStackEntry by navController.currentBackStackEntryAsState()
+                    val currentDestination = navBackStackEntry?.destination
+                    AnimatedVisibility(
+                        visible = topLevelRoutes.any { it.route.hashCode() == currentDestination?.id },
+                    ) {
+                        NavigationBar {
+                            topLevelRoutes.forEach { topLevelRoute ->
+                                NavigationBarItem(
+                                    icon = {
+                                        Icon(
+                                            topLevelRoute.icon,
+                                            contentDescription = topLevelRoute.name
+                                        )
+                                    },
+                                    label = { Text(topLevelRoute.name) },
+                                    selected = currentDestination?.id == topLevelRoute.route.hashCode(),
+                                    onClick = {
+                                        navController.navigate(topLevelRoute.route) {
+                                            popUpTo(navController.graph.findStartDestination().id) {
+                                                saveState = true
+                                            }
+                                            launchSingleTop = true
+                                            restoreState = true
+                                        }
+                                    },
+                                )
+                            }
+                        }
+                    }
+                },
+            ) { innerPadding ->
+                MainNavigation(
+                    navController = navController,
+                    startDestination = AppDestination.Home,
+                    modifier = Modifier
+                        .padding(innerPadding)
+                )
+            }
         }
     }
 }
--- app/src/main/java/com/example/cleanarch/presentation/main/MainNavigation.kt
+++ app/src/main/java/com/example/cleanarch/presentation/main/MainNavigation.kt
@@ -1,24 +1,38 @@
 package com.example.cleanarch.presentation.main
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import com.example.cleanarch.presentation.details.DetailsRoute
 import com.example.cleanarch.presentation.home.HomeRoute
+import com.example.cleanarch.presentation.profile.ProfileRoute
 import kotlinx.serialization.Serializable
 
 @Composable
-fun MainNavigation() {
-    val navController = rememberNavController()
-
-    NavHost(navController = navController, startDestination = AppDestination.Home) {
+fun MainNavigation(
+    navController: NavHostController,
+    startDestination: AppDestination,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier,
+    ) {
         composable<AppDestination.Home> {
             HomeRoute(onNavigateToDetails = { navController.navigate(AppDestination.Details(it)) })
         }
 
         composable<AppDestination.Details> {
-            DetailsRoute(id = 1)
+            val details = it.toRoute<AppDestination.Details>()
+            DetailsRoute(id = details.id)
+        }
+
+        composable<AppDestination.Profile> {
+            ProfileRoute()
         }
     }
 }
@@ -26,6 +40,9 @@ fun MainNavigation() {
 sealed class AppDestination {
     @Serializable
     data object Home : AppDestination()
+
+    @Serializable
+    data object Profile : AppDestination()
 
     @Serializable
     data class Details(val id: Int) : AppDestination()
